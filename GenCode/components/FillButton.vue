662cd60f41568ce809354a30a512a6eee9cb4e0c
Judith Martínez <aguimtz.2003@gmail.com>
2026-01-14 21:59:10 -0600

    Fix:FillButton

--- GenCode/components/FillButton.vue
+++ GenCode/components/FillButton.vue
@@ -1,5 +1,5 @@
 <template>
-     <NuxtLink to="/contacto"
+     <NuxtLink to="/"
                 class="bg-[#0091FF] text-white px-9 py-1 rounded-lg text-lg font-normal hover:bg-transparent transition border-2 border-[#0091FF] hover:text-black">
                 <slot />
             </NuxtLink>
